579ce55a17f2d93c3fe0da6eaed7f1332f485079
Caleb Smith <caleb@fyresmith.com>
2024-03-24 14:38:11 -0400

    update main creator logic

--- main.py
+++ main.py
@@ -2,32 +2,36 @@
 
 from data import *
 from generator import *
+from youtube import *
 
 
-def main():
-    # data = random_data()
+def create_fact_video():
+    data = caption_video_data()
 
-    generate_fact_video(10, {
-        'topic': 'Love Fact',
-        'captions': ["Love is the language that transcends words...", "...it's the unspoken poetry of the heart...", "...and the warmth that envelops your soul."],
-        'channel': '@awesomechannel'
-    })
+    data['channel'] = '@truethoughtsdaily'
 
+    file_path = generate_fact_video(10 if data['type'] == 'THREE' else 7, data)
 
-    # generate_fact_video(12, {
-    #     'topic': data[0].upper(),
-    #     'captions': [data[1], data[2]],
-    #     'channel': '@littlethoughtbites'
-    # })
+    package = {
+        'title': data['captions'][0] + '#shorts',
+        'video': file_path,
+        'description': '#truethoughtsdaily #inspiration #inspirationalquotes  #wisdomquotes #positivity '
+                       '#dailyinspiration #shorts'
+                       '#positivevibes #mindfulness #lifequotes #lovequotes #motivation #quotes #quoteoftheday '
+                       '#quotesaboutlife #positivethinking #thoughtprovoking #thoughtfulreflections #thinking #thoughts'
+                       '#thoughtfortheday #thoughtoftheday #thoughtfortoday #quotes #quotestoliveby #wisdomforliving '
+                       '#wisdomquote',
+        'tags': ['truethoughtsdaily ', 'inspiration ', 'inspirationalquotes  ', 'wisdomquotes ', 'positivity ',
+                 'dailyinspiration ', 'shorts ', 'positivevibes ', 'mindfulness ', 'lifequotes ', 'lovequotes ',
+                 'motivation', 'quotes ', 'quoteoftheday ', 'quotesaboutlife ', 'positivethinking ',
+                 'thoughtprovoking ',
+                 'thoughtfulreflections ', 'thinking ', 'thoughts ', 'thoughtfortheday ', 'thoughtoftheday ',
+                 'thoughtfortoday ', 'quotes ', 'quotestoliveby ', 'wisdomforliving ', 'wisdomquote']
+    }
 
-    data = random_data()
-
-    generate_fact_video(12, {
-        'topic': data[0].upper(),
-        'captions': [data[1], data[2]],
-        'channel': '@littlethoughtbites'
-    })
+    package_video(package)
 
 
 if __name__ == '__main__':
-    main()
+    for i in range(50):
+        create_fact_video()
